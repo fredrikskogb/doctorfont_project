5d883ca70dc364fbd7f3c3b2766480ec23b20246
erik <erik.malsater92@gmail.com>
2018-11-23 13:02:47 +0100

    Add dropdown-menu to navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,6 +3,14 @@
 <header class="navbar">
     <ul class="navbar_linklist">
         <li><a href="<?= $ROOT_URL ?>/index.php">Hem</a></li>
-        <li><a href="<?= $ROOT_URL ?>/about_us.html">Om oss</a></li>
+        <li><a href="<?= $ROOT_URL ?>/views/about_us.html">Om oss</a></li>
+        <li id="navbar_dropdown">
+            <span>Kategorier</span>
+                <div class="navbar_dropdown_content">
+                    <p>Kategori 1</p>
+                    <p>Kategori 2</p>
+                    <p>Kategori 3</p>
+                </div>
+        </li>
     </ul>
 </header>
