b531c5f40a5eac00ce3024c558bbf172ed5e20f3
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-11-23 10:08:41 +0100

    create head.php and beginning of login_view.php

---
+++ views/login_view.php
@@ -0,0 +1,17 @@
+<?php
+include '../includes/head.php';
+?>
+<link rel="stylesheet" type="text/css" href="../includes/style.css">
+</head>
+<body>
+    <main>
+        <div>
+            <img class="view_logo" src="../images/logo_dark.png" alt="millhouse">
+        </div>
+        <div>
+            <div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
