fa5d05b81a6c79026de55d720c7d5f19d6e2002a
erik <erik.malsater92@gmail.com>
2018-11-23 11:19:19 +0100

    create navbar.php and include it in index.php

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<?php ?>
+
+<header class="navbar">
+    <ul class="navbar_linklist">
+        <li><a href="<?= $ROOT_URL ?>/index.php">Hem</a></li>
+        <li><a href="<?= $ROOT_URL ?>/about_us.html">Om oss</a></li>
+    </ul>
+</header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include 'config.php';
+include 'includes/head.php';
+?>
+<title>Millhouse Blog</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+    <body>
+
+        <?php include 'includes/navbar.php';?>
+        
+    </body>
