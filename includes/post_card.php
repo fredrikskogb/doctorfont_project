8c0fd6edb2fe81361a8b588d68f88fde5c214907
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-11-30 14:38:02 +0100

    style post card, feat. Erik

--- includes/post_card.php
+++ includes/post_card.php
@@ -1,15 +1,16 @@
 <?php
     $image = substr($post['image'], 3);
+    $date = substr($post['date'], 0, -8);
 ?>
 
 <div class="post_card">
     
     <h2 class="post_card_title"><?=$post["title"];?></h2>
     <div class="post_card_image_frame">
-        <img src="<?= $image;?>" alt="Bild för inlägget <?=$post['title'];?>">
+        <img class="post_card_image" src="<?= $image;?>" alt="Bild för inlägget <?=$post['title'];?>">
     </div>
     <div class="post_card_description">
         <?=$post["description"];?>
     </div>
-    <p class="post_card_date"><?=$post["date"];?></p>
+    <p class="post_card_date"><?=$date;?></p>
 </div>
