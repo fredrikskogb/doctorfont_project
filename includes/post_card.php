3a3adc09f47aa2ee684be62131f0f5d0510844f6
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-11-30 14:10:13 +0100

    loop out posts on index, feat. Erik

--- includes/post_card.php
+++ includes/post_card.php
@@ -1,10 +1,15 @@
 <?php
-
-echo $post['title'];
-echo $post['description'];
-echo $post['category'];
-echo $post['date'];
-echo $post['created_by'];
+    $image = substr($post['image'], 3);
 ?>
 
-<img src="<?= $post['image'];?>">
+<div class="post_card">
+    
+    <h2 class="post_card_title"><?=$post["title"];?></h2>
+    <div class="post_card_image_frame">
+        <img src="<?= $image;?>" alt="Bild för inlägget <?=$post['title'];?>">
+    </div>
+    <div class="post_card_description">
+        <?=$post["description"];?>
+    </div>
+    <p class="post_card_date"><?=$post["date"];?></p>
+</div>
