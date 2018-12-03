7630743cdefbaf7337120eeb417a8e2a61a4573a
erik <erik.malsater92@gmail.com>
2018-12-03 10:07:37 +0100

    make header image logo responsive

--- includes/post_card.php
+++ includes/post_card.php
@@ -6,13 +6,14 @@
 <div class="post_card">
     
     <h2 class="post_card_title"><?=$post["title"];?></h2>
+    <p class="post_card_date"><?=$date;?></p>
     <div class="post_card_image_frame">
         <img class="post_card_image" src="<?= $image;?>" alt="Bild för inlägget <?=$post['title'];?>">
     </div>
     <div class="post_card_description">
         <?=$post["description"];?>
     </div>
-    <p class="post_card_date"><?=$date;?></p>
+    
 
     <hr class="post_card_border">
             
