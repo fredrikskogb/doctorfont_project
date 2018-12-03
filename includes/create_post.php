2bec5d34606c426af7e6012b387fb51f68569f3c
RebeccaKling <kling.rebecca@hotmail.com>
2018-12-03 10:55:34 +0100

    add new post in index

--- includes/create_post.php
+++ includes/create_post.php
@@ -1,12 +1,8 @@
 <?php
 
 include 'database_connection.php';
-<<<<<<< HEAD
-include '../classes/post.php';
-=======
 include '../classes/post.php';
 
 $create_post = new Post($pdo); 
 
 $create_post->create($_POST['title'], $_FILES['image'], $_POST['description'], $_POST['category']);
->>>>>>> master
