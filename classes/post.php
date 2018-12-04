0ffcb37c151bf3e612766f164345de945d14aa94
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-12-04 15:52:31 +0100

    make create_post_view editable, feat. Erik

--- classes/post.php
+++ classes/post.php
@@ -55,7 +55,7 @@ class Post implements publication
     }
 
     public function update($id){
-        statement = $this->pdo->prepare("UPDATE");
+        $statement = $this->pdo->prepare("UPDATE");
     }
 
     public function getAllPosts(){
--- includes/update_post.php
+++ includes/update_post.php
@@ -3,4 +3,5 @@ include 'database_connection.php';
 include '../classes/post.php';
 
 $edit_post = new Post($pso);
+$edit_post->update();
 
