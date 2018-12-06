956effb2d224b922be1723055af61af432c1318c
erik <erik.malsater92@gmail.com>
2018-12-06 16:47:29 +0100

    fixed delete-button alignment

--- includes/all_comments.php
+++ includes/all_comments.php
@@ -1,11 +1,13 @@
-<div class="single_comment">
+<div class="single_comment_container">
 <?php
 foreach($comment->fetched_comments as $single_fetched_comment){
-
+    ?><div class="single_comment"><?php
     echo $single_fetched_comment['username'] . "<br />";
 
     echo $single_fetched_comment['content'] . "</p>";
-
+    ?><p class="delete_comment_button">DELETE [X]</p> <?php
+    
+    ?></div><?php
     echo "<hr>";
 }
 
