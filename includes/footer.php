1e65c6749cd7571e2bfb103ec1282303213c5a61
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-12-07 11:54:00 +0100

    add social media icons

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,9 @@
 <footer>
+<div class="icons_wrapper_footer">
+    <a href=""><img src="http://localhost/doctorfont_project/images/facebook_color.svg" alt=""></a>
+    <a href=""><img src="http://localhost/doctorfont_project/images/instagram_color.svg" alt=""></a>
+    <a href=""><img src="http://localhost/doctorfont_project/images/twitter_color.svg" alt=""></a>
+</div>
 
 <img class="footer_image" src="<?= $ROOT_URL ?>/images/logo_dark.png" alt="millhouse_logo">
 
