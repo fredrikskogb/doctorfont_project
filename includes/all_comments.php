b3d598f41ca529d024db7690213698d8a63259f6
erik <erik.malsater92@gmail.com>
2018-12-07 12:39:39 +0100

    make delete button only appears to comment belonging to user

--- includes/all_comments.php
+++ includes/all_comments.php
@@ -5,12 +5,18 @@ foreach($comment->fetched_comments as $single_fetched_comment){
     echo $single_fetched_comment['username'] . "<br />";
 
     echo $single_fetched_comment['content'] . "</p>";
-    ?>
+    
+    if ($single_fetched_comment['user_id'] == $_SESSION['user_id']) {
+        ?>
+
     <form action="../includes/delete_comment.php" method="POST">
         <input type="image" src="../images/cross_icon.png" alt="Submit form" class="delete_comment_button" />
         <input type="hidden" name="comment_id" value="<?=$single_fetched_comment['id']?>">
         <input type="hidden" name="post_id" value="<?=$single_fetched_comment['post_id']?>">
     </form>
+
+    <?php
+    }?>
     
     </div><?php
     echo "<hr>";
