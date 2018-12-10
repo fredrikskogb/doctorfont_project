262ca9194139e5b6ddd71b55670e125290c27221
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-12-10 14:04:06 +0100

    display date on comments

--- includes/all_comments.php
+++ includes/all_comments.php
@@ -1,13 +1,18 @@
 <div class="single_comment_container">
 <?php
 foreach($comment->fetched_comments as $single_fetched_comment){
-    ?><div class="single_comment"><?php
-    echo $single_fetched_comment['username'] . "<br />";
+    ?><div class="single_comment">
 
-    echo $single_fetched_comment['content'] . "</p>";
-    if ($single_fetched_comment['user_id'] == $_SESSION['user_id'] || $_SESSION['is_admin']) {
-        ?>
+    <div>
+        <p class="comment_info"><?=$single_fetched_comment['username']?></p>
+        <p class="comment_info" id="comment_date"><?=substr($single_fetched_comment['date'], 0, -3)?></p>
+        <p class="comment_info"><?=$single_fetched_comment['content']?></p>
+    </div>
 
+    <?php
+        if ($single_fetched_comment['user_id'] == $_SESSION['user_id'] || $_SESSION['is_admin']) {
+    ?>
+    
     <form action="../includes/delete_comment.php" method="POST">
         <input type="image" src="../images/cross_icon.png" alt="Submit form" class="delete_comment_button" />
         <input type="hidden" name="comment_id" value="<?=$single_fetched_comment['id']?>">
