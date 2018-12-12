063500210b53902ad38539108e7e553710453634
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2018-12-12 12:38:54 +0100

    make user stay on comment section after publishing a comment

--- includes/create_comment.php
+++ includes/create_comment.php
@@ -8,4 +8,4 @@ $create_post = new Comment($pdo);
 
 $create_post->create($_POST['description'], $_POST['post_id'], $_POST['created_by']);
 
-header ("Location: ../views/comment_view.php?post=". $_POST['post_id']);
+header ("Location: ../views/comment_view.php?post=". $_POST['post_id'] . "#comment");
